c7e0c20723e0c7599585a9d660b503edd690d617
Ben Coleman <colemanb@moravian.edu>
2022-02-02 15:39:32 -0500

    Add script to upload images to imgbb.com

---
+++ toimgbb.py
@@ -0,0 +1,28 @@
+import requests
+import dotenv
+import os
+import base64
+import sys
+
+
+if len(sys.argv) != 2:
+    print('Usage: {} <filename>'.format(sys.argv[0]))
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+dotenv.load_dotenv()
+api_key = os.getenv('APIKEY')
+
+params = {'key': api_key}
+
+with open(filename, 'rb') as img_file:
+    encoded_img = base64.b64encode(img_file.read())
+
+data = {'image': encoded_img}
+
+url = 'https://api.imgbb.com/1/upload'
+
+result = requests.post(url, params=params, data=data)
+
+print(result.json()['data']['url'])
